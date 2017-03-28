cb3440f7c8381255cd93c2d21262dfab6654e593
Student User <student@cti.pub.ro>
2017-03-28 12:02:53 +0300

    modificare aduse de mine

--- labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
+++ labtasks/StartedService/StartedServiceActivity/app/src/main/java/ro/pub/cs/systems/eim/lab05/startedserviceactivity/view/StartedServiceActivity.java
@@ -1,10 +1,11 @@
 package ro.pub.cs.systems.eim.lab05.startedserviceactivity.view;
-
+import ro.pub.cs.systems.eim.lab05.startedserviceactivity.general.Constants;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import android.content.ComponentName;
+import android.content.Intent;
 import ro.pub.cs.systems.eim.lab05.startedserviceactivity.R;
 
 public class StartedServiceActivity extends AppCompatActivity {
@@ -20,12 +21,22 @@ public class StartedServiceActivity extends AppCompatActivity {
 
         messageTextView = (TextView)findViewById(R.id.message_text_view);
 
-        // TODO: exercise 6 - start the service
 
         // TODO: exercise 8a - create an instance of the StartedServiceBroadcastReceiver broadcast receiver
+        messageTextView = (TextView)findViewById(R.id.message_text_view);
+        startedServiceBroadcastReceiver = new StartedServiceBroadcastReceiver(messageTextView);
+
 
         // TODO: exercise 8b - create an instance of an IntentFilter
         // with all available actions contained within the broadcast intents sent by the service
+        startedServiceIntentFilter = new IntentFilter();
+        startedServiceIntentFilter.addAction(Constants.ACTION_STRING);
+        startedServiceIntentFilter.addAction(Constants.ACTION_INTEGER);
+        startedServiceIntentFilter.addAction(Constants.ACTION_ARRAY_LIST);
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("ro.pub.cs.systems.eim.lab05.startedservice", "ro.pub.cs.systems.eim.lab05.startedservice.service.StartedService"));
+        startService(intent);
 
     }
 
